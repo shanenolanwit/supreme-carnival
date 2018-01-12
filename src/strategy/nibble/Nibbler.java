466201240f6c2b2a065833d38a734564f7365557
Shane Nolan <shane.nolan.wit@gmail.com>
2018-01-12 21:11:24 +0000

    adde enum for expression strings

--- src/strategy/nibble/Nibbler.java
+++ src/strategy/nibble/Nibbler.java
@@ -7,32 +7,28 @@ import java.util.regex.Pattern;
 
 public class Nibbler {
 	
-	private final static String A2ZLOWER = "^([a-z]{2,})(.*)$";
-	private final static String A2ZUPPER = "^([A-Z]{2,})(.*)$";
-	private final static String A2ZALL = "^([a-zA-Z]+)(.*)$";
-	private final static String ZERO2NINE = "^([0-9]+)(.*)$";
-	private final static String NONALPHANUMERIC = "^([^a-zA-Z0-9]+)(.*)$";
-	
 	private String regex;
 	
 	public Nibbler(String str){
-		System.out.println("Nibble String is " + str);
+		System.out.println("Nibble String is " + (str.isEmpty() ? "empty" : str));
 		this.regex = "";
-		if(dynoMatch(str, A2ZLOWER).containsKey(true)){
-			regex += "[a-z]{" + dynoMatch(str, A2ZLOWER).get(true).length() + "}";
-			regex += new Nibbler(str.replaceFirst("^[a-z]+", "")).getRegex();
-		} else if(dynoMatch(str, A2ZUPPER).containsKey(true)){
-			regex += "[A-Z]{" + dynoMatch(str, A2ZUPPER).get(true).length() + "}";
-			regex += new Nibbler(str.replaceFirst("^[A-Z]+", "")).getRegex();
-		} else if(dynoMatch(str, A2ZALL).containsKey(true)){
-			regex += "[a-zA-Z]{" + dynoMatch(str, A2ZALL).get(true).length() + "}";
-			regex += new Nibbler(str.replaceFirst("^[a-zA-Z]+", "")).getRegex();
-		} else if(dynoMatch(str, ZERO2NINE).containsKey(true)){
-			regex += "[0-9]{" + dynoMatch(str, ZERO2NINE).get(true).length() + "}";
-			regex += new Nibbler(str.replaceFirst("^[0-9]+", "")).getRegex();
-		} else if(dynoMatch(str, NONALPHANUMERIC).containsKey(true)){
-			regex += "[^a-zA-Z0-9]{" + dynoMatch(str, NONALPHANUMERIC).get(true).length() + "}";
-			regex += new Nibbler(str.replaceFirst("^[^a-zA-Z0-9]+", "")).getRegex();
+		if(str.isEmpty()){
+			//Do nothing
+		} else if(dynoMatch(str, NibbleExpression.A2ZLOWER.getCaptureString()).containsKey(true)){
+			regex += NibbleExpression.A2ZLOWER.getBaseString() + "{" + dynoMatch(str, NibbleExpression.A2ZLOWER.getCaptureString()).get(true).length() + "}";
+			regex += new Nibbler(str.replaceFirst(NibbleExpression.A2ZLOWER.getReplaceString(), "")).getRegex();
+		} else if(dynoMatch(str, NibbleExpression.A2ZUPPER.getCaptureString()).containsKey(true)){
+			regex += NibbleExpression.A2ZUPPER.getBaseString() + "{" + dynoMatch(str, NibbleExpression.A2ZUPPER.getCaptureString()).get(true).length() + "}";
+			regex += new Nibbler(str.replaceFirst(NibbleExpression.A2ZUPPER.getReplaceString(), "")).getRegex();
+		} else if(dynoMatch(str, NibbleExpression.A2ZALL.getCaptureString()).containsKey(true)){
+			regex += NibbleExpression.A2ZALL.getBaseString() + "{" + dynoMatch(str, NibbleExpression.A2ZALL.getCaptureString()).get(true).length() + "}";
+			regex += new Nibbler(str.replaceFirst(NibbleExpression.A2ZALL.getReplaceString(), "")).getRegex();
+		} else if(dynoMatch(str, NibbleExpression.ZERO2NINE.getCaptureString()).containsKey(true)){
+			regex += NibbleExpression.ZERO2NINE.getBaseString() + "{" + dynoMatch(str, NibbleExpression.ZERO2NINE.getCaptureString()).get(true).length() + "}";
+			regex += new Nibbler(str.replaceFirst(NibbleExpression.ZERO2NINE.getReplaceString(), "")).getRegex();
+		} else if(dynoMatch(str, NibbleExpression.NONALPHANUMERIC.getCaptureString()).containsKey(true)){
+			regex += NibbleExpression.NONALPHANUMERIC.getBaseString() + "{" + dynoMatch(str, NibbleExpression.NONALPHANUMERIC.getCaptureString()).get(true).length() + "}";
+			regex += new Nibbler(str.replaceFirst(NibbleExpression.NONALPHANUMERIC.getReplaceString(), "")).getRegex();
 		}		
 	}	
 	
@@ -41,23 +37,19 @@ public class Nibbler {
 	}
 	
 	private Map<Boolean,String> dynoMatch(String str, String expr){
-		System.out.println("[String:" + str + ",Expr:" + expr + "]");
+		
 		Pattern pattern = Pattern.compile(expr);
 		Matcher matcher = pattern.matcher(str);
 		Map<Boolean,String> matchData = new HashMap<Boolean,String>();
 		if(matcher.matches()){
+			System.out.println("[String:" + str + ",Expr:" + expr + ",Match:true]");
 			matchData.put(true, matcher.group(1));
 		}else {
+			System.out.println("[String:" + str + ",Expr:" + expr + ",Match:false]");
 			matchData.put(false, "");
 		}
 		return matchData;
 		
-	}
-	
-	
-	
-	
-	
-	
+	}	
 
 }
--- test/strategy/nibble/NibbleTest.java
+++ test/strategy/nibble/NibbleTest.java
@@ -20,9 +20,12 @@ public class NibbleTest {
 	public void test() {
 		String sample1 = "abc123BatMan999AZ";
 		String sample2 = "abc123BatMan-999AZ";
+		String sample3 = "a99--!1aa";
+		
 				
 		assertEquals("[a-z]{3}[0-9]{3}[a-zA-Z]{6}[0-9]{3}[A-Z]{2}", new Nibbler(sample1).getRegex());
 		assertEquals("[a-z]{3}[0-9]{3}[a-zA-Z]{6}[^a-zA-Z0-9]{1}[0-9]{3}[A-Z]{2}", new Nibbler(sample2).getRegex());
+		assertEquals("[a-zA-Z]{1}[0-9]{2}[^a-zA-Z0-9]{3}[0-9]{1}[a-z]{2}", new Nibbler(sample3).getRegex());
 
 	}
 	
