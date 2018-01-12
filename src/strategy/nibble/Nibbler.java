02c5b7ef0e2f375e4e5de0ade48160ab58a26e62
Shane Nolan <shane.nolan.wit@gmail.com>
2018-01-12 21:39:39 +0000

    updated to use object instead of single entry map

--- src/strategy/nibble/Nibbler.java
+++ src/strategy/nibble/Nibbler.java
@@ -1,55 +1,52 @@
 package strategy.nibble;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Nibbler {
 	
 	private String regex;
 	
 	public Nibbler(String str){
 		System.out.println("Nibble String is " + (str.isEmpty() ? "empty" : str));
-		this.regex = "";
+		this.regex = "";		
 		if(str.isEmpty()){
 			//Do nothing
-		} else if(dynoMatch(str, NibbleExpression.A2ZLOWER.getCaptureString()).containsKey(true)){
-			regex += NibbleExpression.A2ZLOWER.getBaseString() + "{" + dynoMatch(str, NibbleExpression.A2ZLOWER.getCaptureString()).get(true).length() + "}";
-			regex += new Nibbler(str.replaceFirst(NibbleExpression.A2ZLOWER.getReplaceString(), "")).getRegex();
-		} else if(dynoMatch(str, NibbleExpression.A2ZUPPER.getCaptureString()).containsKey(true)){
-			regex += NibbleExpression.A2ZUPPER.getBaseString() + "{" + dynoMatch(str, NibbleExpression.A2ZUPPER.getCaptureString()).get(true).length() + "}";
-			regex += new Nibbler(str.replaceFirst(NibbleExpression.A2ZUPPER.getReplaceString(), "")).getRegex();
-		} else if(dynoMatch(str, NibbleExpression.A2ZALL.getCaptureString()).containsKey(true)){
-			regex += NibbleExpression.A2ZALL.getBaseString() + "{" + dynoMatch(str, NibbleExpression.A2ZALL.getCaptureString()).get(true).length() + "}";
-			regex += new Nibbler(str.replaceFirst(NibbleExpression.A2ZALL.getReplaceString(), "")).getRegex();
-		} else if(dynoMatch(str, NibbleExpression.ZERO2NINE.getCaptureString()).containsKey(true)){
-			regex += NibbleExpression.ZERO2NINE.getBaseString() + "{" + dynoMatch(str, NibbleExpression.ZERO2NINE.getCaptureString()).get(true).length() + "}";
-			regex += new Nibbler(str.replaceFirst(NibbleExpression.ZERO2NINE.getReplaceString(), "")).getRegex();
-		} else if(dynoMatch(str, NibbleExpression.NONALPHANUMERIC.getCaptureString()).containsKey(true)){
-			regex += NibbleExpression.NONALPHANUMERIC.getBaseString() + "{" + dynoMatch(str, NibbleExpression.NONALPHANUMERIC.getCaptureString()).get(true).length() + "}";
-			regex += new Nibbler(str.replaceFirst(NibbleExpression.NONALPHANUMERIC.getReplaceString(), "")).getRegex();
-		}		
+		} else {
+			Nibble lowerNibble = new Nibble(str, NibbleExpression.A2ZLOWER.getCaptureString());			
+			if(lowerNibble.isTasty()){
+				regex += NibbleExpression.A2ZLOWER.getBaseString() + "{" + lowerNibble.getBite().length() + "}";
+				regex += new Nibbler(lowerNibble.getLeftovers()).getRegex();
+			} else {
+				Nibble upperNibble = new Nibble(str, NibbleExpression.A2ZUPPER.getCaptureString());				
+				if(upperNibble.isTasty()){
+					regex += NibbleExpression.A2ZUPPER.getBaseString() + "{" + upperNibble.getBite().length() + "}";
+					regex += new Nibbler(upperNibble.getLeftovers()).getRegex();
+				} else{
+					Nibble lettersNibble = new Nibble(str, NibbleExpression.A2ZALL.getCaptureString());					
+					if(lettersNibble.isTasty()){
+						regex += NibbleExpression.A2ZALL.getBaseString() + "{" + lettersNibble.getBite().length() + "}";
+						regex += new Nibbler(lettersNibble.getLeftovers()).getRegex();
+					} else {
+						Nibble numbersNibble = new Nibble(str, NibbleExpression.ZERO2NINE.getCaptureString());
+						if(numbersNibble.isTasty()){
+							regex += NibbleExpression.ZERO2NINE.getBaseString() + "{" + numbersNibble.getBite().length() + "}";
+							regex += new Nibbler(numbersNibble.getLeftovers()).getRegex();
+						} else {
+							Nibble nonAlphaNibble = new Nibble(str, NibbleExpression.NONALPHANUMERIC.getCaptureString());
+							if(nonAlphaNibble.isTasty()){
+								regex += NibbleExpression.NONALPHANUMERIC.getBaseString() + "{" + nonAlphaNibble.getBite().length() + "}";
+								regex += new Nibbler(nonAlphaNibble.getLeftovers()).getRegex();
+							}
+						} 		
+					}										
+				}				
+			}			
+		}
+
 	}	
 	
 	public String getRegex() {
 		return regex;
 	}
 	
-	private Map<Boolean,String> dynoMatch(String str, String expr){
-		
-		Pattern pattern = Pattern.compile(expr);
-		Matcher matcher = pattern.matcher(str);
-		Map<Boolean,String> matchData = new HashMap<Boolean,String>();
-		if(matcher.matches()){
-			System.out.println("[String:" + str + ",Expr:" + expr + ",Match:true]");
-			matchData.put(true, matcher.group(1));
-		}else {
-			System.out.println("[String:" + str + ",Expr:" + expr + ",Match:false]");
-			matchData.put(false, "");
-		}
-		return matchData;
-		
-	}	
+	
 
 }
