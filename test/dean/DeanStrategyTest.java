90860cd8335665bb08d7a77b518329235da6f809
Dean Gaffney <Dean Gaffney@localhost>
2018-01-12 20:14:16 +0000

    poc added

--- test/dean/DeanStrategyTest.java
+++ test/dean/DeanStrategyTest.java
@@ -1,5 +1,20 @@
 package dean;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class DeanStrategyTest {
+	
+
+	@Test
+	public void test() {
+		System.out.println(DeanStrategy.analyse("abc123abc123"));
+	}
+	
+	@Test
+	public void matcherCountTest() {
+		assertEquals(3, DeanStrategy.getMatchedCount("abc123abc123"));	
+	}
 
 }
