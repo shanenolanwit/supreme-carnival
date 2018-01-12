063f197e0f402ef1d408a4a166b0c7c9729f41ab
Dean Gaffney <Dean Gaffney@localhost>
2018-01-12 23:04:56 +0000

    add comments to methods

--- src/dean/DeanStrategy.java
+++ src/dean/DeanStrategy.java
@@ -45,37 +45,51 @@ public class DeanStrategy {
 				NONALPHANUMERIC));
 	}
 
+	/**
+	 * Analyses a string and generates a regex for the string.
+	 * Method calls analyse(str, regex) with a blank string for regex
+	 * @param str - the string to analyse
+	 * @return regex that matches the given string
+	 */
 	public String analyse(String str) {
 		return analyse(str, "");
 	}
 
+	/**
+	 * Analyses a string and returns  aregex that matches it
+	 * @param str - string to analyse
+	 * @param regex - the current regex expression
+	 * @return regex expression that matches the string
+	 */
 	private String analyse(String str, String regex) {
-
 		if(!str.isEmpty()) {
-			String key = this.regexList.stream()
-					.filter(p -> Pattern.compile(p).matcher(str).matches())
-					.findFirst()
-					.get();
+			String key = this.regexList.stream()		//grab the pattern that matches the string
+									   .filter(p -> Pattern.compile(p).matcher(str).matches())
+									   .findFirst()
+									   .get();		
 
 			Pattern pattern = Pattern.compile(key);
 			Matcher matcher = pattern.matcher(str);
-			String replacementPattern = getReplacementPattern(this.regexMap.get(key));
-			if(matcher.matches()) {
+			String replacementPattern = getReplacementPattern(this.regexMap.get(key));	//create replacement pattern
+			
+			if(matcher.matches()) {	
 				String patternWithCount = replacementPattern + "{" + matcher.group(1).length() + "}";
+				//make recursive call with new string and updated regex
 				return analyse(str.replaceFirst(this.regexMap.get(key), ""), regex += patternWithCount);
 			}
 		}
 		return regex;
 	}
 	
+	/**
+	 * Replaces all characters outside of '[' and ']'
+	 * @param pattern - the pattern to modify
+	 * @return the pattern excluding all characters outside of the square brackets
+	 */
 	private String getReplacementPattern(String pattern) {
 		int start = pattern.indexOf("[");
 		int end = pattern.indexOf("]");
 		return pattern.substring(start, end + 1);
-	}
-
-	public int getMatchedCount(String str) {
-		return 0;
 	}
 
 }
