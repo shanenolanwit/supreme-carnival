2894acd1bfd49a6545860bf0c239cf30c65743ab
Dean Gaffney <Dean Gaffney@localhost>
2018-01-15 10:06:09 +0000

    added sample for email address in DeanTreeTest, passing

--- test/dean/tree/DeanTreeTest.java
+++ test/dean/tree/DeanTreeTest.java
@@ -14,6 +14,7 @@ public class DeanTreeTest {
 		String sample4 = "abc123abc";
 		String sample5 = "123-123-123";
 		String sample6 = "20/03/1996";
+		String sample7 = "dean@gmail.com";
 		
 		assertEquals("([a-z]{2})([A-Z]{5})([a-z]{3})", new DeanTree(sample1).getRegex());				//this passes
 		assertEquals("([A-Z]{5})([a-z]{5})", new DeanTree(sample2).getRegex());							//this passes
@@ -21,6 +22,7 @@ public class DeanTreeTest {
 		assertEquals("([a-z]{3})([0-9]{3})([a-z]{3})", new DeanTree(sample4).getRegex());				//this passes
 		assertEquals("([0-9]{3})([^a-zA-Z0-9]{1})([0-9]{3})([^a-zA-Z0-9]{1})([0-9]{3})", new DeanTree(sample5).getRegex());				//this passes
 		assertEquals("([0-9]{2})([^a-zA-Z0-9]{1})([0-9]{2})([^a-zA-Z0-9]{1})([0-9]{4})", new DeanTree(sample6).getRegex());				//this passes
+		assertEquals("([a-z]{4})([^a-zA-Z0-9]{1})([a-z]{5})([^a-zA-Z0-9]{1})([a-z]{3})", new DeanTree(sample7).getRegex());				//this passes
 	}
 
 }
