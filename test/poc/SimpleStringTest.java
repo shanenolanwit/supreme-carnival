a2a7918bbff87f9ac919db0dcb698fc4601473c0
Shane Nolan <shane.nolan.wit@gmail.com>
2018-01-22 15:44:22 +0000

    began work on similarity utils

--- test/poc/SimpleStringTest.java
+++ test/poc/SimpleStringTest.java
@@ -2,6 +2,8 @@ package poc;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +24,13 @@ public class SimpleStringTest {
 		String sample2 = "abc123";		
 		assertEquals("\\D{3}\\d{3}\\D{6}.{1}\\d{3}", SimpleString.analyse(sample1));
 		assertEquals("\\D{3}\\d{3}", SimpleString.analyse(sample2));
+	}
+	
+	@Test
+	public void testTwo() {
+		List<String> subsets = StringSims.getSubsets("abcd");
+		assertEquals(10, subsets.size());
+		assertEquals("abcd", subsets.get(0));
 	}
 	
 }
